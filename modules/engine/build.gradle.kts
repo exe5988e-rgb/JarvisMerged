89d040bf3b2f3f95817d72d5ba45019b139117f9
amirshams8 <138038547+amirshams8@users.noreply.github.com>
2025-12-14 01:02:37 +0530

    Update build.gradle.kts

--- modules/engine/build.gradle.kts
+++ modules/engine/build.gradle.kts
@@ -1,20 +1,17 @@
-plugins {
-    id("com.android.library")
-    id("org.jetbrains.kotlin.android")
-}
-
 android {
     namespace = "com.jarvismini.engine"
     compileSdk = 34
 
     defaultConfig {
-        minSdk = 26
+        minSdk = 24
         targetSdk = 34
+    }
+
+    kotlinOptions {
+        jvmTarget = "17"
     }
 }
 
 dependencies {
-    // Kotlin coroutines (REQUIRED)
     implementation("org.jetbrains.kotlinx:kotlinx-coroutines-core:1.7.3")
-    implementation("org.jetbrains.kotlinx:kotlinx-coroutines-android:1.7.3")
 }
