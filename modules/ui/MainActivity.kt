d18eda412a22dd23c67d7ddf09d36ba27665ca7e
amirshams8 <138038547+amirshams8@users.noreply.github.com>
2025-12-14 02:08:05 +0530

    Create MainActivity.kt

---
+++ modules/ui/MainActivity.kt
@@ -0,0 +1,12 @@
+package com.jarvismini.ui
+
+import android.app.Activity
+import android.os.Bundle
+import com.jarvismini.ui.R
+
+class MainActivity : Activity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
