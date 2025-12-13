4022de8811549dd8b9548991f19924d6988cfb14
amirshams8 <138038547+amirshams8@users.noreply.github.com>
2025-12-13 20:46:35 +0530

    Update build.gradle.kts

--- modules/automation/build.gradle.kts
+++ modules/automation/build.gradle.kts
@@ -1,8 +1,26 @@
 plugins {
-  id("com.android.library")
-  id("org.jetbrains.kotlin.android")
+    id("com.android.library")
+    id("org.jetbrains.kotlin.android")
 }
-android { namespace = "com.jarvismini.automation"; compileSdk=34; defaultConfig{minSdk=23} }
+
+android {
+    namespace = "com.jarvismini.automation"
+    compileSdk = 34
+
+    defaultConfig {
+        minSdk = 23
+    }
+
+    compileOptions {
+        sourceCompatibility = JavaVersion.VERSION_17
+        targetCompatibility = JavaVersion.VERSION_17
+    }
+
+    kotlinOptions {
+        jvmTarget = "17"
+    }
+}
+
 dependencies {
     implementation(project(":modules:smart"))
     implementation(project(":modules:engine"))
