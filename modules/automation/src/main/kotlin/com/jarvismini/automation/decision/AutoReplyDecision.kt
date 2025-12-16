b703b99f92501452270cbad96be25483b7675b85
exe5988e-rgb <exe5988e@gmail.com>
2025-12-16 09:20:08 +0530

    Delete modules/automation/src/main/kotlin/com/jarvismini/automation/decision/AutoReplyDecision.kt

--- modules/automation/src/main/kotlin/com/jarvismini/automation/decision/AutoReplyDecision.kt
+++
@@ -1,9 +0,0 @@
-package com.jarvismini.automation.decision
-
-sealed interface ReplyDecision
-
-data class AutoReplyDecision(
-    val message: String
-) : ReplyDecision
-
-object NoReplyDecision : ReplyDecision
