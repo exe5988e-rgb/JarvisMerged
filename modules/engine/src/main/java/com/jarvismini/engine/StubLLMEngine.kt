318725a75a94ba92763ab8c025f6065601917292
exe5988e-rgb <exe5988e@gmail.com>
2025-12-16 07:14:32 +0530

    Refactor StubLLMEngine to simplify reply method

--- modules/engine/src/main/java/com/jarvismini/engine/StubLLMEngine.kt
+++ modules/engine/src/main/java/com/jarvismini/engine/StubLLMEngine.kt
@@ -1,18 +1,8 @@
 package com.jarvismini.engine
 
-import android.content.Context
-import com.jarvismini.core.JarvisState
+object StubLLMEngine {
 
-object StubLLMEngine : LLMEngine {
-
-    override fun init(context: Context) {
-        // no-op
-    }
-
-    override fun generateReply(input: AutoReplyInput): String {
-        return ReplyToneResolver.resolve(
-            reason = input.reason,
-            mode = JarvisState.currentMode
-        )
+    fun reply(prompt: String): String {
+        return "Acknowledged. I will inform Mr. Aamir."
     }
 }
