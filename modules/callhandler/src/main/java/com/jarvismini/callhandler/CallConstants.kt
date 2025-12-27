c2c3f57cb3d9da0766561991ade902a249596b3e
amirshams8 <138038547+amirshams8@users.noreply.github.com>
2025-12-27 17:56:43 +0530

    Delete modules/callhandler/src/main/java/com/jarvismini/callhandler directory

--- modules/callhandler/src/main/java/com/jarvismini/callhandler/CallConstants.kt
+++
@@ -1,6 +0,0 @@
-package com.jarvismini.callhandler
-
-object CallConstants {
-    const val TAG = "CALL-HANDLER"
-    const val AUTO_REPLY_TEXT = "I’m currently unavailable. Will call you back soon."
-}
