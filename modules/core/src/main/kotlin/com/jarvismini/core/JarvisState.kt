ca705fb983d61f28912d484e68620b5b563ba86b
amirshams8 <138038547+amirshams8@users.noreply.github.com>
2025-12-27 16:48:27 +0530

    Update JarvisState.kt

--- modules/core/src/main/kotlin/com/jarvismini/core/JarvisState.kt
+++ modules/core/src/main/kotlin/com/jarvismini/core/JarvisState.kt
@@ -1,5 +1,25 @@
 package com.jarvismini.core
 
+import android.content.Context
+
 object JarvisState {
+
+    @Volatile
     var currentMode: JarvisMode = JarvisMode.NORMAL
+        private set
+
+    fun init(context: Context) {
+        val prefs = context.getSharedPreferences("jarvis", Context.MODE_PRIVATE)
+        currentMode = JarvisMode.valueOf(
+            prefs.getString("mode", JarvisMode.NORMAL.name)!!
+        )
+    }
+
+    fun setMode(context: Context, mode: JarvisMode) {
+        currentMode = mode
+        context.getSharedPreferences("jarvis", Context.MODE_PRIVATE)
+            .edit()
+            .putString("mode", mode.name)
+            .apply()
+    }
 }
