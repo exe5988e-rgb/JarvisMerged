3cdf8f14e23e7665b674d396c468e5f4344ab17b
amirshams8 <138038547+amirshams8@users.noreply.github.com>
2026-01-10 07:49:53 +0530

    Delete modules/ui directory

--- modules/ui/src/main/kotlin/com/jarvismini/ui/ViewBindings.kt
+++
@@ -1,7 +0,0 @@
-package com.jarvismini.ui
-
-import android.app.Activity
-import android.view.View
-
-// Simple helper extension to keep your UI clean
-fun <T : View> Activity.bind(id: Int): Lazy<T> = lazy { findViewById(id) }
