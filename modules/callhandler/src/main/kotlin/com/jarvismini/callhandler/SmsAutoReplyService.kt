5b11765639207cdfd1fbb368b6889d83536a04f5
amirshams8 <138038547+amirshams8@users.noreply.github.com>
2026-01-23 18:05:38 +0530

    Update SmsAutoReplyService.kt

--- modules/callhandler/src/main/kotlin/com/jarvismini/callhandler/SmsAutoReplyService.kt
+++ modules/callhandler/src/main/kotlin/com/jarvismini/callhandler/SmsAutoReplyService.kt
@@ -6,7 +6,7 @@ import android.util.Log
 object SmsAutoReplyService {
 
     private const val AUTO_REPLY =
-        "Hello Aamir sir, Jarvis here. I’ll get back to you shortly."
+        "Hello from Aamir sir, Jarvis here. He’ll get back to you shortly."
 
     fun reply(context: Context, number: String, incomingText: String) {
         SmsSender.send(context, number, AUTO_REPLY)
