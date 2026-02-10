b0895b3175b3098922fa9af38646e2ae378147e4
exe5988e-rgb <exe5988e@gmail.com>
2026-02-11 03:13:26 +0530

    Update MainScreen.kt

--- app/src/main/kotlin/com/jarvismini/ui/main/MainScreen.kt
+++ app/src/main/kotlin/com/jarvismini/ui/main/MainScreen.kt
@@ -7,7 +7,7 @@ import com.jarvismini.core.progress.*
 import com.jarvismini.core.routine.RoutineProvider
 import com.jarvismini.ui.boot.BootScreen
 import com.jarvismini.ui.home.EnhancedHomeScreen
-import com.jarvismini.ui.JarvisChatScreen
+import com.jarvismini.ui.chat.JarvisChatScreen
 import com.jarvismini.ui.calendar.CalendarViewModel
 import com.jarvismini.ui.calendar.DayCalendarScreen
 import com.jarvismini.ui.settings.SettingsScreen
