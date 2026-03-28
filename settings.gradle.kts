76df49beb584c7f7cc0bdb17ea050e69e33043b8
amirshams8 <138038547+amirshams8@users.noreply.github.com>
2026-03-28 17:55:09 +0530

    Update settings.gradle.kts

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -5,12 +5,26 @@ pluginManagement {
         gradlePluginPortal()
     }
 }
+
 dependencyResolutionManagement {
-    repositoriesMode.set(RepositoriesMode.FAIL_ON_PROJECT_REPOS)
+    repositoriesMode.set(RepositoriesMode.PREFER_SETTINGS)
     repositories {
         google()
         mavenCentral()
     }
 }
-rootProject.name = "SyncWave"
+
+plugins {
+    id("com.android.application") version "8.3.1" apply false
+    id("com.android.library") version "8.3.1" apply false
+    id("org.jetbrains.kotlin.android") version "1.9.22" apply false
+}
+
+rootProject.name = "JarvisMerged"
+
 include(":app")
+include(":modules:core")
+include(":modules:engine")
+include(":modules:automation")
+include(":modules:smart")
+include(":modules:callhandler")
